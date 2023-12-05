93cdfff0da1a9c5a61d108f5b97349764893ac83
Quentin Verdier <quent1.verdier@gmail.com>
2023-12-05 11:24:51 +0100

    Add chosen language and greeting

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 import unittest
+from datetime import datetime
+
+# Prompts the user to choose a language between fr and en
+def choose_language():
+    while True:
+        user_input = input("Choose your language (fr/en): ").lower()
+        if user_input in ["fr", "en"]:
+            return user_input
+        else:
+            print("Invalid choice. Please enter 'fr' for French or 'en' for English.")
+
+def greeting(lang):
+    current_hour = datetime.now().hour
+    if lang == "en":
+        if 5 <= current_hour < 12:
+            print("morning")
+        elif 12 <= current_hour < 18:
+            print("afternoon")
+        else:
+            print("evening")
+    else:
+        if 5 <= current_hour < 16:
+            print("bonjour")
+        elif 16 <= current_hour < 22:
+            print("bonsoir")
+        else:
+            print("bienvenue")
 
 # Reverses the given string.
 def reverse_string(input_string):
@@ -10,4 +37,5 @@ class TestReverseStringFunction(unittest.TestCase):
         self.assertEqual(reverse_string("racecar"), "racecar")
 
 if __name__ == '__main__':
-    unittest.main()
+    chosen_language = choose_language()
+    greeting(chosen_language)
