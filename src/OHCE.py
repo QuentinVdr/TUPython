d40846998d1c17167cf5177474bfb94a55fc5e25
Quentin Verdier <quent1.verdier@gmail.com>
2024-01-31 16:14:07 +0100

    ✅ Add "bien dit" to palindrome check method to OHCE class

--- src/OHCE.py
+++ src/OHCE.py
@@ -1,3 +1,10 @@
+import os
+
+
 class OHCE:
-    def miroir(chaine):
+    @classmethod
+    def est_palindrome(cls, chaine):
+        mirror = chaine[::-1]
+        if mirror == chaine:
+            return chaine + os.linesep + "Bien dit !"
         return chaine[::-1]
