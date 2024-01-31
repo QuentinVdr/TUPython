ff95cc249dccefb73afcaa73b12ef0c1ad00f53d
Quentin Verdier <quent1.verdier@gmail.com>
2024-01-31 15:54:47 +0100

    🧪 Refactor miroir test case in OHCE_test.py

--- test/OHCE_test.py
+++ test/OHCE_test.py
@@ -2,12 +2,14 @@ import unittest
 
 from src.OHCE import OHCE
 
+
 class OHCETest(unittest.TestCase):
     def test_miroir(self):
-      # ÉTANT DONNE une chaîne de caractères
-      chaine = "test"
-      # QUAND je la passe à la fonction miroir
-      result = OHCE.miroir(chaine)
-      # ALORS je reçois la chaîne de caractères inversée
-      attendu = chaine[::-1]
-      self.assertEqual(result, attendu)
+        # ÉTANT DONNE une chaîne de caractères
+        for chaine in ["test", "miroir"]:
+            with self.subTest(chaine):
+                # QUAND je la passe à la fonction miroir
+                resultat = OHCE.miroir(chaine)
+                # ALORS je reçois la chaîne de caractères inversée
+                attendu = chaine[::-1]
+                self.assertEqual(resultat, attendu)
