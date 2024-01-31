d6e0c649b6578a1672085b57bcdcd453b530bdb5
Quentin Verdier <quent1.verdier@gmail.com>
2024-01-31 16:28:58 +0100

    ✅ Refactor palindrome response to fix OHCE test

--- src/OHCE.py
+++ src/OHCE.py
@@ -5,6 +5,7 @@ class OHCE:
     @classmethod
     def est_palindrome(cls, chaine):
         mirror = chaine[::-1]
+        response = "Bonjour" + os.linesep + mirror
         if mirror == chaine:
-            return chaine + os.linesep + "Bien dit !"
-        return chaine[::-1]
+            response += os.linesep + "Bien dit !"
+        return response
