3e61e793bee7f1b4a061bada59d0edc68ca94308
Quentin VERDIER <quent1.verdier@gmail.com>
2024-02-06 22:18:21 +0100

    ✨ Add moment and refactor language initialization in main.py

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import locale
+import time
 
 from src.langueAnglais import LangueAnglais
 from src.langueFrancais import LangueFrancaise
+from src.moment import Moment
 from src.OHCE import OHCE
+
+
+# get current system hour
+def get_system_hour():
+    # This will return the current hour
+    return int(time.strftime("%H"))
+
+
+# get moment for init OHCE
+def get_moment():
+    hour = get_system_hour()
+    if 6 <= hour < 12:
+        return Moment.MATIN
+    elif 12 <= hour < 19:
+        return Moment.APRES_MIDI
+    elif 19 <= hour < 22:
+        return Moment.SOIR
+    elif 22 <= hour < 24 or 0 <= hour < 6:
+        return Moment.NUIT
+    else:
+        return Moment.INCONNUE
 
 
 # get language of user's system
@@ -15,15 +38,23 @@ def get_system_language():
     return system_lang
 
 
-if __name__ == "__main__":
+# get langue for init OHCE
+def get_language():
     # set up OHCE with user language
     if get_system_language() == "fr":
-        ohce = OHCE(langue=LangueFrancaise(), moment=None)
+        return LangueFrancaise()
     # use english language as default language
     # cause english is international language
     # so user with unsupported language will get english
     else:
-        ohce = OHCE(langue=LangueAnglais(), moment=None)
+        return LangueAnglais()
+
+
+if __name__ == "__main__":
+    language = get_language()
+    moment = get_moment()
+
+    ohce = OHCE(langue=language, moment=moment)
 
     # get text input of user
     user_input = input("Enter a string : ")
